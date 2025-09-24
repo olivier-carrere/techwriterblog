730d390569d245a2850c112bd6ad46645bdd1816
Olivier Carrère <carrereo@gmail.com>
2025-09-24 15:44:39 +0200

    Revert "Install API endpoint"
    This reverts commit 8a2a312a0fd3077f2d550c1d76e1554d0b9cef28.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,16 @@ import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
-import yaml from "@rollup/plugin-yaml";
+import yaml from "@rollup/plugin-yaml"; // 👈 add this
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  output: "server", // ✅ required for API routes on Vercel
   integrations: [mdx(), sitemap(), icon()],
   vite: {
     plugins: [
       tailwindcss(),
-      yaml(), // ✅ enable YAML import support
+      yaml(), // 👈 enable YAML import support
     ],
   },
 });
